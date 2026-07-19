45f890f33cb136c0609310e0a8cbb7c466f1745d
Umer Majeed <umarmajeed786k@gmail.com>
2026-07-19 12:20:54 +0500

    Hero Update

--- components/hero/HeroImage.tsx
+++ components/hero/HeroImage.tsx
@@ -1,46 +1,11 @@
 "use client";
 
+import HeroOrb from "./HeroOrb";
+
 export default function HeroImage() {
   return (
-    <div className="relative flex items-center justify-center">
-      {/* Outer Ring */}
-
-      <div
-        className="
-          h-[430px]
-          w-[430px]
-          rounded-full
-          border
-          border-cyan-400/20
-        "
-      />
-
-      {/* Inner Ring */}
-
-      <div
-        className="
-          absolute
-          h-[300px]
-          w-[300px]
-          rounded-full
-          border
-          border-cyan-400/30
-        "
-      />
-
-      {/* Core */}
-
-      <div
-        className="
-          absolute
-          h-32
-          w-32
-          rounded-full
-          bg-cyan-400
-          blur-2xl
-          opacity-60
-        "
-      />
+    <div className="flex items-center justify-center">
+      <HeroOrb />
     </div>
   );
 }
--- components/hero/HeroOrb.tsx
+++ components/hero/HeroOrb.tsx
@@ -1,39 +1,122 @@
 "use client";
 
-import OrbCore from "./OrbCore";
-import OrbGlow from "./OrbGlow";
-import OrbRing from "./OrbRing";
+import { motion } from "framer-motion";
 
 export default function HeroOrb() {
   return (
-    <div
-      className="
-        relative
-        flex
-        h-[520px]
-        w-[520px]
-        items-center
-        justify-center
-      "
-    >
-      <OrbGlow />
+    <div className="relative flex items-center justify-center">
 
-      <OrbRing
-        size={420}
-        duration={40}
+      {/* Ambient Glow */}
+
+      <motion.div
+        animate={{
+          scale: [1, 1.12, 1],
+          opacity: [0.25, 0.45, 0.25],
+        }}
+        transition={{
+          duration: 5,
+          repeat: Infinity,
+          ease: "easeInOut",
+        }}
+        className="
+          absolute
+          h-[260px]
+          w-[260px]
+          rounded-full
+          bg-cyan-400/20
+          blur-[90px]
+        "
       />
 
-      <OrbRing
-        size={330}
-        duration={28}
+      {/* Outer Ring */}
+
+      <motion.div
+        animate={{
+          rotate: 360,
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 28,
+          ease: "linear",
+        }}
+        className="
+          absolute
+          h-[420px]
+          w-[420px]
+          rounded-full
+          border
+          border-white/10
+        "
       />
 
-      <OrbRing
-        size={240}
-        duration={18}
+      {/* Middle Ring */}
+
+      <motion.div
+        animate={{
+          rotate: -360,
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 18,
+          ease: "linear",
+        }}
+        className="
+          absolute
+          h-[320px]
+          w-[320px]
+          rounded-full
+          border
+          border-cyan-400/20
+        "
       />
 
-      <OrbCore />
+      {/* Inner Ring */}
+
+      <motion.div
+        animate={{
+          rotate: 360,
+          scale: [1, 1.04, 1],
+        }}
+        transition={{
+          rotate: {
+            repeat: Infinity,
+            duration: 10,
+            ease: "linear",
+          },
+          scale: {
+            repeat: Infinity,
+            duration: 4,
+          },
+        }}
+        className="
+          absolute
+          h-[220px]
+          w-[220px]
+          rounded-full
+          border
+          border-white/20
+        "
+      />
+
+      {/* Core */}
+
+      <motion.div
+        animate={{
+          scale: [1, 1.12, 1],
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 2,
+        }}
+        className="
+          h-28
+          w-28
+          rounded-full
+          bg-cyan-400
+          shadow-[0_0_80px_rgba(0,245,255,.5)]
+        "
+      />
+
     </div>
   );
 }
